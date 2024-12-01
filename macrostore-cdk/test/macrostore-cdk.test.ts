1685757afeb0a71ddf2d4174210537ecac1dc5e2
danihp6 <danihp6@gmail.com>
2024-12-01 18:11:57 +0100

    add clothing-store, electronics-store and macrostore-cdk

---
+++ macrostore-cdk/test/macrostore-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MacrostoreCdk from '../lib/macrostore-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/macrostore-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MacrostoreCdk.MacrostoreCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
