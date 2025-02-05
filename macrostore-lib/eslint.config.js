88c9495f04461f957dd77568ad22546a227a9d66
danihp6 <danihp6@gmail.com>
2025-02-05 20:16:29 +0100

    rule off

--- macrostore-lib/eslint.config.js
+++ macrostore-lib/eslint.config.js
@@ -30,6 +30,8 @@ module.exports = [
   {
     files: ['**/*.html'],
     // Override or add rules here
-    rules: {},
+    rules: {
+      '@typescript-eslint/ban-ts-comment': 'off'
+    },
   },
 ];
